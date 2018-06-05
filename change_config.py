f07febbf814c75c9aa5f5bce2a3c86558622fdcf
ppetrou <petrakisp13@gmail.com>
2018-06-05 21:12:38 +0300

    add premilinary Spark Structure Streaming Support predictions (each point per "row") are saved as parquet files Spark Structure Streaming Implementation uses flatMapGroupsWithState Dstreams are saved as parquet files Dstreams uses statefule streaming add premilinary evaluation support add new cleansed dataset change data types, support only 2D prediction

--- change_config.py
+++ change_config.py
@@ -11,13 +11,13 @@ window = [
     ('10', '9')]
 
 path = [
-    ('./data/blmd_4s.csv', '4000'),
-    ('./data/london_4s.csv', '4000'),
-    ('./data/blmd_8s.csv', '8000'),
-    ('./data/london_8s.csv', '8000'),
+    ('blmd_4s.csv', '4000'),
+    ('london_4s.csv', '4000'),
+    ('blmd_8s.csv', '8000'),
+    ('london_8s.csv', '8000'),
 ]
 
-horizon=['1', '2', '4', '8', '16']
+horizon=['32']
 
 for w in window:
     for p in path:
@@ -27,28 +27,28 @@ for w in window:
             config.set('Spark', 'spark.train_set', w[1])
 
             config.set('Spark', 'spark.path', p[0])
-            config.set('Spark', 'spark.path', p[1])
+            config.set('Spark', 'spark.sampling', p[1])
 
             config.set('Spark', 'spark.horizon', h)
             config.write(cfgfile)
             cfgfile.close()
 
-            os.system('spark-submit --master local[*] '
+            os.system('timeout 11m spark-submit --master local[*] --driver-memory 12g '
                       '--properties-file "./config/onlinearima.properties" '
                       '--class prediction.arma.OArimaGD '
-                      './target/OnlineArimaTrajectory-jar-with-dependencies.jar csv')
+                      './target/OnlineArimaTrajectory-jar-with-dependencies.jar ')
 
-            os.system('spark-submit --master local[*] '
+            os.system('timeout 11m spark-submit --master local[*] --driver-memory 12g '
                       '--properties-file "./config/onlinearima.properties" '
                       '--class prediction.arima.OArimaGD '
-                      './target/OnlineArimaTrajectory-jar-with-dependencies.jar csv')
+                      './target/OnlineArimaTrajectory-jar-with-dependencies.jar ')
 
-            os.system('spark-submit --master local[*] '
+            os.system('timeout 11m spark-submit --master local[*] --driver-memory 12g '
                       '--properties-file "./config/onlinearima.properties" '
                       '--class prediction.arma.OArimaNS '
-                      './target/OnlineArimaTrajectory-jar-with-dependencies.jar csv')
+                      './target/OnlineArimaTrajectory-jar-with-dependencies.jar ')
 
-            os.system('spark-submit --master local[*] '
+            os.system('timeout 11m spark-submit --master local[*] --driver-memory 12g '
                       '--properties-file "./config/onlinearima.properties" '
                       '--class prediction.arima.OArimaNS '
-                      './target/OnlineArimaTrajectory-jar-with-dependencies.jar csv')
+                      './target/OnlineArimaTrajectory-jar-with-dependencies.jar ')
